c3fbb8de40c5c50b2e5f3916c4027ee699eb6de8
shrey kataria <shreykataria1@gmail.com>
2024-06-27 21:31:13 +0530

    added cluster and os module for load balancing

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
-// initialize
-require("dotenv").config();
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
+const os = require("os");
 
-// imports
-const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoute");
-const bodyParser = require("body-parser");
-const productRoutes = require("./routes/productsRoute");
-const orderRoutes = require("./routes/orderRoute");
+// load balancing
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master ${process.pid} is running`);
 
-// host
-const port = process.env.PORT || 8000;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// db
-connectDB();
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  // initialize
+  require("dotenv").config();
+  const express = require("express");
+  const app = express();
 
-// middlewares
-app.use(express.json());
-app.use(bodyParser.json());
+  // imports
+  const connectDB = require("./config/db");
+  const userRoutes = require("./routes/userRoute");
+  const bodyParser = require("body-parser");
+  const productRoutes = require("./routes/productsRoute");
+  const orderRoutes = require("./routes/orderRoute");
+  const limiter = require("./middlewares/rateLimit");
+  const errorHandler = require("./middlewares/errorHandler");
+  const { mockPayPal, mockStripe } = require("./mocks/paymentMocks");
+  const mockLogistics = require("./mocks/logisticMocks");
+  const fs = require("fs");
+  const https = require("https");
 
-// routes
-app.use("/api", userRoutes);
-app.use("/api", productRoutes);
-app.use("/api", orderRoutes);
+  // host
+  const PORT = process.env.PORT || 8000;
 
-// server
-app.listen(port, () => {
-  console.log(`app is listening to port : ${port}`);
-});
+  // db
+  connectDB();
+
+  // middlewares
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(errorHandler);
+  app.use(limiter);
+
+  // nock apis
+  mockPayPal();
+  mockStripe();
+  mockLogistics();
+
+  // routes
+  app.use("/api", userRoutes);
+  app.use("/api", productRoutes);
+  app.use("/api", orderRoutes);
+
+  // server
+  if (process.env.NODE_ENV === "production") {
+    const options = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+
+    https.createServer(options, app).listen(PORT, () => {
+      console.log(
+        `https Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      );
+    });
+  } else {
+    app.listen(PORT, () => {
+      console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      );
+    });
+  }
+  console.log(`Worker ${process.pid} started`);
+}
